20f0e10d8d5f134ad360b2317903504a3e46d8cd
Vasylina <Vorobey_Alinka@ukr.net>
2024-06-09 15:51:59 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,43 @@
+console.log('#13. JavaScript homework example file')
+
+/*
+ * #1
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідною електронною адресою за допомогою регулярного виразу.
+ * Функція повертає true, якщо електронна адреса валідна, і false в іншому випадку.
+ *
+ */
+
+function isValidEmail(email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+}
+
+console.log(isValidEmail('example@example.com'))
+console.log(isValidEmail('invalid-email'))
+// console.log(isValidEmail('example@example.com')) // Повинно вивести: true
+// console.log(isValidEmail('invalid-email'))       // Повинно вивести: false
+
+/*
+ * #2
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідним URL веб-сайту за допомогою регулярного виразу.
+ * Функція повертає true, якщо URL валідний, і false в іншому випадку.
+ *
+ */
+
+/*
+
+*/
+
+function isValidUrl(url) {
+  const regex =  /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  return regex.test(url);
+}
+console.log(isValidUrl('https://www.example.com'))
+console.log(isValidUrl('invalid-url'))
+// console.log(isValidUrl('https://www.example.com')) // Повинно вивести: true
+// console.log(isValidUrl('invalid-url'))             // Повинно вивести: false
+
+// Експорт функції для використання та тестування
+export { isValidEmail, isValidUrl }
